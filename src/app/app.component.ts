74382bc2e2de7af940752eab97fbe3f758cd9109
kh-khalil <khaled.m.hassan.94@gmail.com>
2024-03-04 03:30:44 +0200

    global styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ItemCardComponent } from './item-card/item-card.component';
+import { MainCardComponent } from './main-card/main-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [RouterOutlet, ItemCardComponent, MainCardComponent, CommonModule],
 })
 export class AppComponent {
   title = 'luftborn-fe';
