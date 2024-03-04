0a4639ba23da22da456e4ee3e0605541457e67ec
kh-khalil <khaled.m.hassan.94@gmail.com>
2024-03-04 07:16:13 +0200

    removing comments

--- src/app/item-card/item-card.component.ts
+++ src/app/item-card/item-card.component.ts
@@ -11,13 +11,4 @@ import { Item } from '../services/models/data.model';
 })
 export class ItemCardComponent {
   @Input() item!: Item;
-
-  // getBackgroundColor(): string {
-  //   if (this.item.id === 1 || 4) return '#ff8b64';
-  //   if (this.item.id === 2 || 5) return '#55C2E6';
-  //   if (this.item.id === 6 || 9) return '#4BCF82';
-  //   if (this.item.id === 7 || 10) return '#7335D2';
-  //   if (this.item.id === 3) return '#FF5E7D';
-  //   return 'F1C75B';
-  // }
 }
