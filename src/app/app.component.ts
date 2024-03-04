94e8fdadfcb2630ebd4c02132375459365cce5d1
kh-khalil <khaled.m.hassan.94@gmail.com>
2024-03-04 07:12:14 +0200

    Updating bg colors by processing input data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnDestroy } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Observable, Subject, takeUntil } from 'rxjs';
+import { Observable } from 'rxjs';
 import { ItemCardComponent } from './item-card/item-card.component';
 import { MainCardComponent } from './main-card/main-card.component';
 import { ApiDataService } from './services/api/data.service';
@@ -14,24 +14,13 @@ import { Item } from './services/models/data.model';
   styleUrl: './app.component.scss',
   imports: [RouterOutlet, ItemCardComponent, MainCardComponent, CommonModule],
 })
-export class AppComponent implements OnDestroy {
+export class AppComponent {
   title = 'luftborn-fe';
   list = new Array(10);
 
   data$: Observable<Item[]>;
 
-  private destroy$ = new Subject();
-
-  ngOnDestroy(): void {
-    this.destroy$.next(null);
-    this.destroy$.unsubscribe();
-  }
   constructor(private apiDataService: ApiDataService) {
     this.data$ = this.apiDataService.getData();
-    // .pipe(takeUntil(this.destroy$))
-    // .subscribe((res) => {
-    //   this.data = res;
-    //   console.log('data', this.data);
-    // });
   }
 }
--- src/app/services/api/data.service.ts
+++ src/app/services/api/data.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { Item } from '../models/data.model';
 
 @Injectable({
@@ -10,8 +10,50 @@ export class ApiDataService {
   constructor(private httpClient: HttpClient) {}
 
   getData(): Observable<Item[]> {
-    return this.httpClient.get<Item[]>(
-      'https://fakestoreapi.com/products?limit=10.'
-    );
+    return this.httpClient
+      .get<Item[]>('https://fakestoreapi.com/products?limit=10.')
+      .pipe(
+        map((response) => {
+          return this.processData(response);
+        })
+      );
+  }
+
+  processData(response: Item[]): Item[] {
+    const items: Array<Item> = [];
+
+    response?.forEach((item, index) => {
+      let bgColor;
+      switch (index + 1) {
+        case 1:
+        case 4:
+          bgColor = '#ff8b64';
+          break;
+        case 2:
+        case 5:
+          bgColor = '#55C2E6';
+          break;
+        case 6:
+        case 9:
+          bgColor = '#4BCF82';
+          break;
+        case 7:
+        case 10:
+          bgColor = '#7335D2';
+          break;
+        case 3:
+          bgColor = '#FF5E7D';
+          break;
+
+        default:
+          bgColor = '#F1C75B';
+          break;
+      }
+      return items.push({
+        ...item,
+        bgColor,
+      });
+    });
+    return items;
   }
 }
